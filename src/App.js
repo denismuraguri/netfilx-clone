c211c19677bb91492276bf198b9b9510facf8e32
denosy <50819494+denosy@users.noreply.github.com>
2021-05-05 13:24:56 +0300

    share

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import Jumbotron from "./components/jumbotron";
+import { JumbotronContainer } from "./containers/jumbotron";
 import JumboData from "./fixtures/jumbo";
 
 function App() {
   return (
-    <div className="App">
-      <Jumbotron.Container>
-        {JumboData.map((item) => (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron>
-        ))}
-      </Jumbotron.Container>
-    </div>
+      <JumbotronContainer />
   );
 }
 
