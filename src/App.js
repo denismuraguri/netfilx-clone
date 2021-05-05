4f976d358a250601c48f645a29a98c0a1c3b6c7c
denosy <50819494+denosy@users.noreply.github.com>
2021-05-05 10:18:53 +0300

    share

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React  from "react";
+import Jumbotron from "./components/jumbotron";
+import JumboData from './fixtures/jumbo'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Jumbotron.Container>
+        {JumboData.map((item)=> (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron>
+        ))}
+        
+      </Jumbotron.Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'normalize.css'
+import { GlobalStyles } from './global-styles';
+
 ReactDOM.render(
-    <App />,
+  <>
+  <GlobalStyles />
+    <App />
+    </>,
   document.getElementById('root')
 );
 
